721aa79b8a86821100a5d4d98fc74b8d9ee5ea08
Yiwen Wang <yiwenwang9702@gmail.com>
2019-04-11 21:44:17 -0400

    image-to-json

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # A simple program to extract colors from a picture. 
 
 Combination of http requests and image manipulation in Golang.
+
+Ongoing. The image-to-json part is done.
--- gocolors.go
+++ gocolors.go
@@ -1,18 +1,64 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"image"
+	"log"
+	"os"
 
-	"github.com/parnurzeal/gorequest"
+	// "github.com/parnurzeal/gorequest"
+	_ "image/jpeg"
 )
 
+type color [4]uint32
+type newcolor [3]int
+
+func rbgaToRgb(c color) newcolor {
+	var converted newcolor
+	a := float64(c[3]) / 32768
+	r := (float64(c[0]) * a) / 256
+	g := (float64(c[1]) * a) / 256
+	b := (float64(c[2]) * a) / 256
+	converted[0] = int(r)
+	converted[1] = int(g)
+	converted[2] = int(b)
+	return converted
+}
 func main() {
-	request := gorequest.New()
-	resp, body, errs := request.Get("https://www.youtube.com/").End()
-	if errs != nil {
-		panic(errs)
+	var pixel color
+	// request := gorequest.New()
+	// resp, body, errs := request.Get("https://www.youtube.com/").End()
+	// if errs != nil {
+	// 	panic(errs)
+	// }
+	// if resp.StatusCode == 200 {
+	// 	fmt.Println(body[120:140])
+	// }
+	reader, err := os.Open("test_pics/IMG_1036.JPG")
+	if err != nil {
+		log.Fatal(err)
 	}
-	if resp.StatusCode == 200 {
-		fmt.Println(body[120:140])
+	defer reader.Close()
+	m, _, err := image.Decode(reader)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	data := make(map[string]interface{})
+	data["model"] = "default"
+	input := make([]newcolor, 0, 3)
+	bounds := m.Bounds()
+	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+		for x := bounds.Min.X; x < bounds.Max.X; x++ {
+			r, g, b, a := m.At(x, y).RGBA()
+			pixel = [4]uint32{r, g, b, a}
+			convertedpixel := rbgaToRgb(pixel)
+			input = append(input, convertedpixel)
+		}
+	}
+	data["input"] = input
+
+	js, _ := json.Marshal(data)
+	fmt.Println(string(js)[len(js)-50:])
 }
