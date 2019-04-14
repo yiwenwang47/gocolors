25b50c88597fc02bfbe83e076657b0545238decc
Yiwen Wang <yiwenwang9702@gmail.com>
2019-04-13 22:51:39 -0400

    basic approach fix

--- gocolors.go
+++ gocolors.go
@@ -2,10 +2,10 @@ package gocolors
 
 //Simply returns a slice of colors in RGB.
 //The basic approach: blur, resize, kmeans cluster.
-func ExtractRGB(filename string, k int, alpha float64) {
+func ExtractRGB(filename string, k int, alpha float64) []Newcolor {
 	img := blurAndResize(filename)
 	imgSlice := imageToSlice(img)
-	clustered := extractByKmeans(mSlice, k, alpha)
+	clustered := extractByKmeans(imgSlice, k, alpha)
 	return clustered
 }
 
